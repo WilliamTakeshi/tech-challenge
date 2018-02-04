21e6e6d92a7ca34309898a94e39f3e45dc0b3c66
ramondelemos <ramondelemos@gmail.com>
2018-02-04 18:31:50 -0200

    Configurando o Travis CI para replicar os commits no branch dev para o branch master quando todos os testes passarem.

---
+++ travis_merge_script.sh
@@ -0,0 +1,10 @@
+if [ "$TRAVIS_BRANCH" != "dev" ]; then 
+    exit 0;
+fi
+export GIT_COMMITTER_EMAIL="ramondelemos@gmail.com"
+export GIT_COMMITTER_NAME="Ramon de Lemos"
+git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
+git fetch --all || exit
+git checkout master || exit
+git merge --no-ff "$TRAVIS_COMMIT" || exit
+git push @github.com/">https://${GITHUB_TOKEN}@github.com/ramondelemos/tech-challenge.git
