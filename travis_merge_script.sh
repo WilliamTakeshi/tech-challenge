34a2758b5a71688c503ffbb001cc35dae85e9251
ramondelemos <ramondelemos@gmail.com>
2018-02-04 20:46:35 -0200

    Adição do commit do Travis CI.

--- travis_merge_script.sh
+++ travis_merge_script.sh
@@ -7,4 +7,5 @@ git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 git checkout master || exit
 git merge --no-ff "$TRAVIS_COMMIT" || exit
+git commit --amend -am "Travis CI commit."
 git push @github.com/">https://${GITHUB_TOKEN}@github.com/ramondelemos/tech-challenge.git
