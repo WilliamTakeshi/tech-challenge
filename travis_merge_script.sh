9379f266b99778ac772a623f6325ee9e3d4884e8
ramondelemos <ramondelemos@gmail.com>
2018-02-05 00:31:11 -0200

    Reconfiguração do github.

--- travis_merge_script.sh
+++ travis_merge_script.sh
@@ -4,8 +4,6 @@ fi
 export GIT_COMMITTER_EMAIL="ramondelemos@gmail.com"
 export GIT_COMMITTER_NAME="Ramon de Lemos"
 rev=$(git rev-parse --short HEAD)
-git config user.name "ramondelemos@gmail.com"
-git config user.password ${GITHUB_PWD}
 git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/* || exit
 git fetch --all || exit
 git stash
@@ -14,5 +12,4 @@ git merge --no-ff "$TRAVIS_COMMIT" || exit
 git stash pop
 msg=$(git log --format=%B -n1)
 git commit --amend -am "${msg} - commit from Travis CI - rev: ${rev}"
-#git push https://${GITHUB_TOKEN}@github.com/ramondelemos/tech-challenge.git
-git push origin master
+git push https://${GITHUB_TOKEN}@github.com/ramondelemos/tech-challenge.git
