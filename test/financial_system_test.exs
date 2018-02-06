a27bee55cfb449d65ea5293160586ee8335c78f9
ramondelemos <ramondelemos@gmail.com>
2018-02-06 21:48:46 -0200

    Colocando o modulo FinancialSystemTest para refletir a realidade dos testes.

--- test/financial_system_test.exs
+++ test/financial_system_test.exs
@@ -3,26 +3,26 @@ defmodule FinancialSystemTest do
   doctest FinancialSystem
 
   test "User should be able to transfer money to another account" do
-    assert true
+    assert false
   end
 
   test "User cannot transfer if not enough money available on the account" do
-    assert true
+    assert false
   end
 
   test "A transfer should be cancelled if an error occurs" do
-    assert true
+    assert false
   end
 
   test "A transfer can be splitted between 2 or more accounts" do
-    assert true
+    assert false
   end
 
   test "User should be able to exchange money between different currencies" do
-    assert true
+    assert false
   end
 
   test "Currencies should be in compliance with ISO 4217" do
-    assert true
+    assert false
   end
 end
