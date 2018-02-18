c11b78aa5ad2a01c52c00bd78e8d818d745298f3
ramondelemos <ramondelemos@gmail.com>
2018-02-18 12:55:56 -0300

    Adição e configuração do :ex_doc.

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule FinancialSystem.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+  @github_url "https://github.com/ramondelemos/tech-challenge"
+
   def project do
     [
       app: :financial_system,
-      version: "0.1.0",
+      name: "FinancialSystem",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
+      docs: docs(),
       test_coverage: [tool: ExCoveralls]
     ]
   end
@@ -23,7 +30,32 @@ defmodule FinancialSystem.Mixfile do
   defp deps do
     [
       {:excoveralls, "~> 0.8.1", only: [:dev, :test]},
-      {:credo, "~> 0.8.10", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.8.10", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.18", only: [:dev, :docs], runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Tech Challenge Elixir.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Ramon de Lemos"],
+      contributors: ["Ramon de Lemos"],
+      links: %{"GitHub" => @github_url}
+    ]
+  end
+
+  def docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: @github_url,
+      main: "FinancialSystem",
+      extras: ["README.md"]
     ]
   end
 end
