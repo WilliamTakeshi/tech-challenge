11cf956f27b9185138af076d1b38c3dcbb13837a
ramondelemos <ramondelemos@gmail.com>
2018-03-03 16:02:51 -0300

    Definição do métodos new/3 e new!/3 e seus testes.

--- lib/account.ex
+++ lib/account.ex
@@ -16,4 +16,33 @@ defmodule Account do
           balance: Dinheiro.t(),
           transactions: list()
         }
+
+  @spec new(String.t(), Dinheiro.t(), NaiveDateTime.t()) ::
+          {:ok, t()} | {:error, String.t()}
+  @doc """
+  Create a new `Account` struct.
+
+  ## Example:
+        iex> {:ok, date_time} = NaiveDateTime.new(~D[2018-03-23], ~T[13:59:07.005])
+        iex> {:ok, money} = Dinheiro.new(12345, :BRL)
+        iex> Account.new("Ramon de Lemos", money, date_time)
+        {:ok, %Account{user: "Ramon de Lemos", balance: %Dinheiro{amount: 1234500, currency: :BRL}, transactions: [%AccountTransaction{date_time: ~N[2018-03-23 13:59:07.005], value: %Dinheiro{amount: 1234500, currency: :BRL}}]}}
+
+  """
+  def new(user, amount, date_time) do
+  end
+
+  @spec new!(String.t(), Dinheiro.t(), NaiveDateTime.t()) :: t()
+  @doc """
+  Create a new `Account` struct.
+
+  ## Example:
+        iex> {:ok, date_time} = NaiveDateTime.new(~D[2018-03-23], ~T[13:59:07.005])
+        iex> {:ok, money} = Dinheiro.new(12345, :BRL)
+        iex> Account.new("Ramon de Lemos", money, date_time)
+        %Account{user: "Ramon de Lemos", balance: %Dinheiro{amount: 1234500, currency: :BRL}, transactions: [%AccountTransaction{date_time: ~N[2018-03-23 13:59:07.005], value: %Dinheiro{amount: 1234500, currency: :BRL}}]}
+
+  """
+  def new!(user, amount, date_time) do
+  end
 end
---
+++ test/account_test.exs
@@ -0,0 +1,64 @@
+defmodule AccountTest do
+  use ExUnit.Case
+  doctest Account
+
+  test "new/2" do
+    user = "Ramon de Lemos"
+    date_time = NaiveDateTime.utc_now()
+    value = Dinheiro.new!(12345, :BRL)
+    transactions = [%AccountTransaction{date_time: date_time, value: value}]
+
+    assert Account.new(user, value, date_time) ==
+             {:ok,
+              %Account{user: user, balance: value, transactions: transactions}}
+
+    assert Account.new(5, value, date_time) ==
+             {:error, ":user must be a String"}
+
+    assert Account.new(user, value, "2018-03-23 08:40:07.005") ==
+             {:error, ":date_time must be a NaiveDateTime struct"}
+
+    assert Account.new(user, 5, date_time) ==
+             {:error, ":balance must be a Dinheiro struct"}
+
+    assert Account.new(
+             user,
+             %Dinheiro{
+               amount: 600,
+               currency: :NONE
+             },
+             date_time
+           ) == {:error, "'NONE' does not represent an ISO 4217 code"}
+  end
+
+  test "new!/2" do
+    user = "Ramon de Lemos"
+    date_time = NaiveDateTime.utc_now()
+    value = Dinheiro.new!(12345, :BRL)
+    transactions = [%AccountTransaction{date_time: date_time, value: value}]
+
+    assert Account.new!(user, value, date_time) ==
+             %Account{user: user, balance: value, transactions: transactions}
+
+    assert_raise ArgumentError, fn ->
+      Account.new(5, value, date_time)
+    end
+
+    assert_raise ArgumentError, fn ->
+      Account.new(user, value, "2018-03-23 08:40:07.005")
+    end
+
+    assert_raise ArgumentError, fn -> Account.new(user, 5, date_time) end
+
+    assert_raise ArgumentError, fn ->
+      Account.new(
+        user,
+        %Dinheiro{
+          amount: 600,
+          currency: :NONE
+        },
+        date_time
+      )
+    end
+  end
+end
