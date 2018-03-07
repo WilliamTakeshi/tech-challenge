bf58cb10ed5f69747db783b0d6be72f125ad020f
ramondelemos <ramondelemos@gmail.com>
2018-03-06 23:42:48 -0300

    Definição dos métodos transfer/3, exchange/3 e exchange!/3.

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -2,4 +2,30 @@ defmodule FinancialSystem do
   @moduledoc """
   Documentation for FinancialSystem.
   """
+
+  @spec transfer(Account.t(), Account.t() | list(), Dinheiro.t()) ::
+          {:ok, Account.t(), Account.t() | list()} | {:error, String.t()}
+  @doc """
+  Transfers a value from one account to one or more accounts.
+  """
+  def transfer(from, to, value) do
+  end
+
+  @spec exchange(Dinheiro.t(), atom(), float()) ::
+          {:ok, Dinheiro.t()} | {:error, String.t()}
+  @doc """
+  Exchange one currency value to another.
+  """
+  def exchange(from, to, exchange_rate) do
+    {:ok, exchange!(from, to, exchange_rate)}
+  rescue
+    e -> {:error, e.message}
+  end
+
+  @spec exchange!(Dinheiro.t(), atom(), float()) :: Dinheiro.t()
+  @doc """
+  Exchange one currency value to another.
+  """
+  def exchange!(from, to, exchange_rate) do
+  end
 end
