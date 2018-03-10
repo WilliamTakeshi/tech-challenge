e51d91ab906bd0719e80d58a74f0a1a6f89aec03
ramondelemos <ramondelemos@gmail.com>
2018-03-10 16:06:56 -0300

    Implementação do método transfer!/3.

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -21,6 +21,63 @@ defmodule FinancialSystem do
   Transfers a value from one account to one or more accounts.
   """
   def transfer!(from, to, value) do
+    do_transfer!(from, to, value)
+  end
+
+  defp do_transfer!(from, to, value) when is_map(to) do
+    {f, t} = do_transfer!(from, [%{account: to, ratio: 1}], value)
+    {f, List.first(t)}
+  end
+
+  defp do_transfer!(from, to, value) when is_list(to) do
+    debit =
+      NaiveDateTime.utc_now()
+      |> AccountTransaction.new!(Dinheiro.multiply!(value, -1))
+
+    debit_account = Account.execute!(from, debit)
+
+    ratios =
+      to
+      |> Enum.map(fn t -> t.ratio end)
+
+    credit_accounts =
+      to
+      |> Enum.map(fn t -> t.account end)
+      |> get_credits(Dinheiro.divide!(value, ratios))
+      |> Enum.map(fn {account, value} -> execute_async(account, value) end)
+      |> Enum.map(&Task.await/1)
+      |> get_async_returns!()
+
+    {debit_account, credit_accounts}
+  end
+
+  defp get_credits([account_head | account_tail], [value_head | value_tail]) do
+    [{account_head, value_head} | get_credits(account_tail, value_tail)]
+  end
+
+  defp get_credits([], []), do: []
+
+  defp execute_async(account, value) do
+    Task.async(fn -> do_execute(account, value) end)
+  end
+
+  defp do_execute(account, value) do
+    {:ok,
+     Account.execute!(
+       account,
+       AccountTransaction.new!(NaiveDateTime.utc_now(), value)
+     )}
+  rescue
+    e -> {:error, e}
+  end
+
+  defp get_async_returns!([]), do: []
+
+  defp get_async_returns!([head | tail]) do
+    case head do
+      {:ok, value} -> [value | get_async_returns!(tail)]
+      {:error, reason} -> raise reason
+    end
   end
 
   @spec exchange(Dinheiro.t(), atom(), float()) ::
