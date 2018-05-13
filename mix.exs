eb80857a1206cb675fb377c45c15d669042924f7
ramondelemos <ramondelemos@gmail.com>
2018-05-13 19:44:58 -0300

    Atualização para a versão 0.2.0.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule FinancialSystemApi.Mixfile do
   use Mix.Project
 
-  @version "0.1.2"
+  @version "0.2.0"
   @github_url "https://github.com/ramondelemos/tech-challenge"
 
   def project do
