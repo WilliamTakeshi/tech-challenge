fa040e4ee9ee41f221fa2d65364a199d3af5d09f
ramondelemos <ramondelemos@gmail.com>
2018-05-14 00:58:12 -0300

    Removendo ecto.create do teste.

--- mix.exs
+++ mix.exs
@@ -91,7 +91,7 @@ defmodule FinancialSystemApi.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"],
+      test: ["ecto.migrate", "test"],
       pre_build: [
         "format",
         "credo --strict"
