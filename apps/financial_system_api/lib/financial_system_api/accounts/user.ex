073153af6744d7d5da1dbbd56db560a8862dd160
ramondelemos <ramondelemos@gmail.com>
2018-05-19 11:44:42 -0300

    Adding accounts context.

---
+++ apps/financial_system_api/lib/financial_system_api/accounts/user.ex
@@ -0,0 +1,39 @@
+defmodule FinancialSystemApi.Accounts.User do
+  @moduledoc false
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field(:email, :string)
+    field(:email_verified, :boolean, default: false)
+    field(:name, :string)
+    field(:password, :string, virtual: true)
+    field(:password_hash, :string)
+    field(:token, :string)
+    field(:username, :string)
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [
+      :name,
+      :email,
+      :username,
+      :password_hash,
+      :email_verified,
+      :token
+    ])
+    |> validate_required([
+      :name,
+      :email,
+      :username,
+      :password_hash,
+      :email_verified,
+      :token
+    ])
+  end
+end
