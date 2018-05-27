3181f0316afa0c450f6ef06447b5bb1433560adb
ramondelemos <ramondelemos@gmail.com>
2018-05-27 14:25:02 -0300

    Aplicando validação da moeda da conta a ser criada.

--- apps/financial_system_api/lib/financial_system_api/accounts/account_resolver.ex
+++ apps/financial_system_api/lib/financial_system_api/accounts/account_resolver.ex
@@ -2,13 +2,22 @@ defmodule FinancialSystemApi.Accounts.AccountResolver do
   @moduledoc false
 
   alias FinancialSystemApi.Accounts
+  alias FinancialSystemApi.FinancialSystemWrapper
 
   import FinancialSystemApi.Resolvers
 
   def create(args, %{context: %{current_user: %{id: id}}}) do
-    %{amount: 0.0, currency: args.currency, user_id: id}
-    |> Accounts.create_account()
-    |> response()
+    currency = %{amount: 0.0, currency: args.currency}
+
+    case FinancialSystemWrapper.validate_currency(currency) do
+      {:ok, value} ->
+        %{amount: 0.0, currency: value, user_id: id}
+        |> Accounts.create_account()
+        |> response()
+
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
   def create(_args, _info) do
