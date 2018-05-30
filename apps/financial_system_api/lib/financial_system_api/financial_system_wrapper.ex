ae8e82003ef61c88b59bf352d4ff75f2da3f897d
ramondelemos <ramondelemos@gmail.com>
2018-05-30 00:30:11 -0300

    Refatoração para utilização do AccountBuilder na execução do FinancialSystem.transfer/3.

--- apps/financial_system_api/lib/financial_system_api/financial_system_wrapper.ex
+++ apps/financial_system_api/lib/financial_system_api/financial_system_wrapper.ex
@@ -16,45 +16,48 @@ defmodule FinancialSystemApi.FinancialSystemWrapper do
     new_to = build_account(to)
 
     case FinancialSystem.transfer(new_from, new_to, money) do
-      {:ok, f, t}
-        ->  {
-              :ok,
-              from: %PersistentAccount{
-                amount: Dinheiro.to_float!(f.balance),
-                currency:  Atom.to_string(f.balance.currency),
-                user_id: from.user_id
-              },
-              to: %PersistentAccount{
-                amount: Dinheiro.to_float!(t.balance),
-                currency:  Atom.to_string(t.balance.currency),
-                user_id: to.user_id
-              }
+      {:ok, f, t} ->
+        {
+          :ok,
+          %{
+            from: %PersistentAccount{
+              amount: Dinheiro.to_float!(f.balance),
+              currency: Atom.to_string(f.balance.currency),
+              user_id: from.user_id
+            },
+            to: %PersistentAccount{
+              amount: Dinheiro.to_float!(t.balance),
+              currency: Atom.to_string(t.balance.currency),
+              user_id: to.user_id
             }
-      {:error, reason}
-        -> {:error, reason}
+          }
+        }
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
   defp build_account(account) do
-    transactions = account.transactions || []
+    transactions =
+      case is_list(account.transactions) do
+        true -> account.transactions
+        false -> []
+      end
 
-    %Account{
-      user: "#{account.id}",
-      balance: Dinheiro.new!(account.amount, account.currency),
-      transactions: build_transactions(transactions, account.currency)
-    }
+    %AccountBuilder{}
+    |> AccountBuilder.set_user("#{account.id}")
+    |> AccountBuilder.set_currency(account.currency)
+    |> AccountBuilder.set_balance(account.amount)
+    |> build_transactions(transactions)
+    |> AccountBuilder.build!()
   end
 
-  defp build_transaction(transaction, currency) do
-    %AccountTransaction{
-      date_time: transaction.date_time,
-      value: Dinheiro.new!(transaction.value, currency)
-    }
+  defp build_transactions(builder, [head | tail]) do
+    builder
+    |> builder.set_transaction(head.date_time, head.value)
+    |> build_transactions(tail)
   end
 
-  defp build_transactions([head | tail], currency) do
-    [build_transaction(head, currency) | build_transactions(tail, currency)]
-  end
-
-  defp build_transactions([], _currency), do: []
+  defp build_transactions(builder, []), do: builder
 end
