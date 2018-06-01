822a944758156975347134935e97d48d2b493ca2
ramondelemos <ramondelemos@gmail.com>
2018-06-01 13:28:30 -0300

    Persistindo as alterações na base de dados.

--- apps/financial_system_api/lib/financial_system_api/accounts/account_resolver.ex
+++ apps/financial_system_api/lib/financial_system_api/accounts/account_resolver.ex
@@ -7,17 +7,17 @@ defmodule FinancialSystemApi.Accounts.AccountResolver do
   import FinancialSystemApi.Resolvers
 
   def create(args, %{context: %{current_user: %{id: id}}}) do
-    currency = %{amount: 0.0, currency: args.currency}
-
-    case FinancialSystemWrapper.validate_currency(currency) do
-      {:ok, value} ->
-        %{amount: 0.0, currency: value, user_id: id}
+    case FinancialSystemWrapper.create(id, 0.0, args.currency) do
+      {:ok, account} ->
+        account
         |> Accounts.create_account()
         |> response()
 
       {:error, reason} ->
         {:error, reason}
     end
+  rescue
+    e -> {:error, e.message}
   end
 
   def create(_args, _info) do
@@ -33,11 +33,25 @@ defmodule FinancialSystemApi.Accounts.AccountResolver do
       if from.id == to.id do
         {:error, "you can not transfer money to same account"}
       else
-        FinancialSystemWrapper.transfer(from, to, args.value)
+        case FinancialSystemWrapper.transfer(from, to, args.value) do
+          {:ok, result} ->
+            case Accounts.update_transfer(from, result.from, to, result.to) do
+              {:ok, {:ok, f, t}} ->
+                {:ok, %{from: f, to: t}}
+
+              {:error, reason} ->
+                {:error, reason}
+            end
+
+          {:error, reason} ->
+            {:error, reason}
+        end
       end
     else
       {:error, "the origin account does not belongs to you"}
     end
+  rescue
+    e -> {:error, e.message}
   end
 
   def transfer(_args, _info) do
@@ -48,10 +62,15 @@ defmodule FinancialSystemApi.Accounts.AccountResolver do
     from = Accounts.get_account!(args.from)
 
     if from.user_id == id do
-      FinancialSystemWrapper.withdraw(from, args.value)
+      case FinancialSystemWrapper.withdraw(from, args.value) do
+        {:ok, f} -> Accounts.update_account(from, f)
+        {:error, reason} -> {:error, reason}
+      end
     else
       {:error, "this account does not belongs to you"}
     end
+  rescue
+    e -> {:error, e.message}
   end
 
   def withdraw(_args, _info) do
