6795db8cdb068a9bdf80d612a319adb9caa46f21
ramondelemos <ramondelemos@gmail.com>
2018-06-01 13:23:20 -0300

    Refatorando os testes do módulo AccountBuilder.

--- apps/financial_system/test/account_builder_test.exs
+++ apps/financial_system/test/account_builder_test.exs
@@ -4,13 +4,15 @@ defmodule AccountBuilderTest do
   setup do
     date_time = NaiveDateTime.utc_now()
     user = "Ramon de Lemos"
-    value = Dinheiro.new!(0, :BRL)
+    value = Dinheiro.new!(0.00, :BRL)
     transaction = %AccountTransaction{date_time: date_time, value: value}
 
     empty_account = %Account{
       user: user,
       balance: value,
-      transactions: [transaction]
+      transactions: [
+        transaction
+      ]
     }
 
     {:ok,
@@ -27,9 +29,30 @@ defmodule AccountBuilderTest do
              context.user
              |> AccountBuilder.set_user()
              |> AccountBuilder.set_currency(:BRL)
-             |> AccountBuilder.set_balance(0)
-             |> AccountBuilder.set_transaction(context.date_time, 0)
+             |> AccountBuilder.set_balance(0.00)
+             |> AccountBuilder.set_transactions([
+               %{
+                 date_time: context.date_time,
+                 value: 0.00
+               }
+             ])
              |> AccountBuilder.build!()
+  end
+
+  test "building an account with empty list of transactions", context do
+    account =
+      context.user
+      |> AccountBuilder.set_user()
+      |> AccountBuilder.set_currency(:BRL)
+      |> AccountBuilder.set_balance(0.00)
+      |> AccountBuilder.set_transactions([])
+      |> AccountBuilder.build!()
+
+    assert context.empty_account.user == account.user
+    assert Dinheiro.equals?(context.empty_account.balance, account.balance)
+
+    assert length(context.empty_account.transactions) ==
+             length(account.transactions)
   end
 
   test "building an account with balance different of the sum of transactions values",
@@ -40,7 +63,12 @@ defmodule AccountBuilderTest do
              |> AccountBuilder.set_user()
              |> AccountBuilder.set_currency(:BRL)
              |> AccountBuilder.set_balance(1)
-             |> AccountBuilder.set_transaction(context.date_time, 0)
+             |> AccountBuilder.set_transactions([
+               %{
+                 date_time: context.date_time,
+                 value: 0
+               }
+             ])
              |> AccountBuilder.build()
   end
 
@@ -76,7 +104,7 @@ defmodule AccountBuilderTest do
              context.user
              |> AccountBuilder.set_user()
              |> AccountBuilder.set_currency(:BRL)
-             |> AccountBuilder.set_transaction("NONE", 1)
+             |> AccountBuilder.set_transactions([%{date_time: "NONE", value: 1}])
              |> AccountBuilder.build()
   end
 
@@ -85,7 +113,12 @@ defmodule AccountBuilderTest do
              context.user
              |> AccountBuilder.set_user()
              |> AccountBuilder.set_currency(:BRL)
-             |> AccountBuilder.set_transaction(context.date_time, "NONE")
+             |> AccountBuilder.set_transactions([
+               %{
+                 date_time: context.date_time,
+                 value: "NONE"
+               }
+             ])
              |> AccountBuilder.build()
   end
 end
