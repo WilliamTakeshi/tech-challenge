8df42fad17b93d795fd94f29ccf8d65e1957241c
ramondelemos <ramondelemos@gmail.com>
2018-06-02 23:13:52 -0300

    Atualização para a versão 0.1.7.

--- rel/config.exs
+++ rel/config.exs
@@ -45,7 +45,7 @@ end
 # will be used by default
 
 release :financial_system_api do
-  set version: "0.1.6"
+  set version: "0.1.7"
   set applications: [
     :runtime_tools,
     financial_system: :permanent,
