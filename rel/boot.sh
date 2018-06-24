2fbffc744880155337eb498580cccd082a4dd480
ramondelemos <ramondelemos@gmail.com>
2018-06-24 13:10:16 -0300

    Obtendo o ip do host da aplicação para envio das métricas.

--- rel/boot.sh
+++ rel/boot.sh
@@ -2,6 +2,7 @@
 set -e
 
 export RANCHER_IP=$(wget -qO- http://rancher-metadata.rancher.internal/latest/self/container/primary_ip)
+export RANCHER_HOST_IP=$(wget -qO- http://rancher-metadata.rancher.internal/latest/self/host/agent_ip)
 export RANCHER_SERVICE_NAME=$(wget -qO- http://rancher-metadata.rancher.internal/latest/self/service/name)
 
 /opt/app/bin/financial_system_api $@
