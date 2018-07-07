c9df56f6772423c58c066cb31fff257aa6b262fc
ramondelemos <ramondelemos@gmail.com>
2018-07-07 09:53:34 -0300

    Atualizado a versão do postgresql no travis.ci.

--- apps/financial_system_api/priv/repo/migrations/20180701224236_create_aggregation_structure.exs
+++ apps/financial_system_api/priv/repo/migrations/20180701224236_create_aggregation_structure.exs
@@ -6,8 +6,7 @@ defmodule FinancialSystemApi.Repo.Migrations.CreateAggregationStructure do
     do_execute("insert_into_rollup_table.sql")
     do_execute("create_incremental_rollup_window_function.sql")
     do_execute("create_transactions_1day_table.sql")
-    #do_execute("create_do_transactions_aggregations_function.sql")
-    run_sql()
+    do_execute("create_do_transactions_aggregations_function.sql")
   end
 
   def down do
@@ -21,41 +20,5 @@ defmodule FinancialSystemApi.Repo.Migrations.CreateAggregationStructure do
     |> Path.join("repo/scripts/#{file_name}")
     |> File.read!()
     |> execute()
-  end
-
-  defp run_sql do
-  query = """
-  CREATE OR REPLACE FUNCTION
-  do_transactions_aggregations(OUT start_id bigint, OUT end_id bigint)
-  RETURNS record
-  LANGUAGE plpgsql
-  AS $function$
-  BEGIN
-      /* determine which page views we can safely aggregate */
-      SELECT window_start, window_end INTO start_id, end_id
-      FROM incremental_rollup_window('transactions_1day_rollup');
-
-      /* exit early if there are no new page views to aggregate */
-      IF start_id > end_id THEN RETURN; END IF;
-
-      /* aggregate the page views, merge results if the entry already exists */
-      INSERT INTO transactions_1day (transaction_day, currency, credit, debit)
-        select date_trunc('day', t.date_time)
-            , c.currency
-            , sum(GREATEST(t.value, 0)) as credit
-            , abs(sum(LEAST(t.value, 0))) as debit       
-          from transactions as t
-        inner join accounts as c on c.id = t.account_id
-        where t.id between start_id and end_id
-        group by date_trunc('day', t.date_time), c.currency
-      ON CONFLICT (transaction_day, currency) DO
-        UPDATE SET credit = transactions_1day.credit + EXCLUDED.credit
-                , debit = transactions_1day.debit + EXCLUDED.debit;
-  END;
-  $function$;
-  """
-
-  query
-  |> execute()
   end
 end
