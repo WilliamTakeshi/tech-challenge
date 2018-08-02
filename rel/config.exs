844e447ff4137d98c77831ae41bdda3972d98c5b
Ramon de Lemos <ramondelemos@gmail.com>
2018-08-02 14:19:15 -0300

    Atualização para a versão "0.2.3".

--- rel/config.exs
+++ rel/config.exs
@@ -45,7 +45,7 @@ end
 # will be used by default
 
 release :financial_system_api do
-  set version: "0.2.2"
+  set version: "0.2.3"
   set applications: [
     :runtime_tools,
     financial_system: :permanent,
